db2450203cefc8213d1f2533838c825f0b7f2a04
Alexey Sosnin <sosnin.alexey@gmail.com>
2015-03-15 11:32:55 +0300

    Settings page - vertical scroll

--- src/Inoreader/Inoreader/Inoreader.WindowsPhone/Views/Pages/SettingsPage.xaml.cs
+++ src/Inoreader/Inoreader/Inoreader.WindowsPhone/Views/Pages/SettingsPage.xaml.cs
@@ -1,16 +1,30 @@
-﻿using Windows.UI.Xaml.Navigation;
+﻿using Windows.Graphics.Display;
+using Windows.UI.Xaml;
 using Microsoft.Practices.Prism.Mvvm;
 
 namespace Inoreader.Views.Pages
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
 	public sealed partial class SettingsPage : IView
 	{
 		public SettingsPage()
 		{
-			this.InitializeComponent();
+			InitializeComponent();
+		}
+
+		private void SettingsPage_OnLoaded(object sender, RoutedEventArgs e)
+		{
+			ItemsLayoutRoot.Width = Window.Current.Bounds.Width - 10;
+			DisplayInformation.GetForCurrentView().OrientationChanged += SettingsPage_OrientationChanged;
+		}
+
+		private void SettingsPage_OnUnloaded(object sender, RoutedEventArgs e)
+		{
+			DisplayInformation.GetForCurrentView().OrientationChanged -= SettingsPage_OrientationChanged;
+		}
+
+		void SettingsPage_OrientationChanged(DisplayInformation sender, object args)
+		{
+			ItemsLayoutRoot.Width = Window.Current.Bounds.Width - 10;
 		}
 	}
 }
