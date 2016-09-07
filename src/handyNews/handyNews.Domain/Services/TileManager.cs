946bb71baa225fc2c3af8de41100a285580fc4d8
Alexey Sosnin <sosnin.alexey@outlook.com>
2016-09-08 02:11:23 +0300

    update NuGet packages

--- src/handyNews/handyNews.Domain/Services/TileManager.cs
+++ src/handyNews/handyNews.Domain/Services/TileManager.cs
@@ -19,10 +19,10 @@ namespace handyNews.Domain.Services
 {
     public class TileManager : ITileManager
     {
-        private const string DrawCanvasName = "DrawCanvas";
-        private const string WideFileName = "WideTile.png";
-        private const string SquareFileName = "SquareTile.png";
-        private const string SquareSmallFileName = "SquareSmallTile.png";
+        private const string DRAW_CANVAS_NAME = "DrawCanvas";
+        private const string WIDE_FILE_NAME = "WideTile.png";
+        private const string SQUARE_FILE_NAME = "SquareTile.png";
+        private const string SQUARE_SMALL_FILE_NAME = "SquareSmallTile.png";
 
         private readonly ITelemetryManager _telemetryManager;
 
@@ -44,62 +44,62 @@ namespace handyNews.Domain.Services
                 {
                     return;
                 }
-                var wideName = await SaveFileAsync(wideImage, WideFileName, 310U, 150U);
+                var wideName = await SaveFileAsync(wideImage, WIDE_FILE_NAME, 310U, 150U);
 
                 var squareImage = await RenderAsync(new StartTileSquare(count), 150, 150);
                 if (squareImage == null)
                 {
                     return;
                 }
-                var squareName = await SaveFileAsync(squareImage, SquareFileName, 150U, 150U);
+                var squareName = await SaveFileAsync(squareImage, SQUARE_FILE_NAME, 150U, 150U);
 
                 var squareSmallImage = await RenderAsync(new StartTileSquareSmall(count), 71, 71);
                 if (squareSmallImage == null)
                 {
                     return;
                 }
-                var squareSmallName = await SaveFileAsync(squareSmallImage, SquareSmallFileName, 71U, 71U);
+                var squareSmallName = await SaveFileAsync(squareSmallImage, SQUARE_SMALL_FILE_NAME, 71U, 71U);
 
 
                 var tile = new TileContent
-                           {
-                               Visual = new TileVisual
-                                        {
-                                            TileWide = new TileBinding
-                                                       {
-                                                           Branding = TileBranding.Name,
-                                                           Content = new TileBindingContentAdaptive
-                                                                     {
-                                                                         BackgroundImage = new TileBackgroundImage
-                                                                                           {
-                                                                                               Source = new TileImageSource("ms-appdata:///local/" + wideName)
-                                                                                           }
-                                                                     }
-                                                       },
-                                            TileMedium = new TileBinding
-                                                         {
-                                                             Branding = TileBranding.Name,
-                                                             Content = new TileBindingContentAdaptive
-                                                                       {
-                                                                           BackgroundImage = new TileBackgroundImage
-                                                                                             {
-                                                                                                 Source = new TileImageSource("ms-appdata:///local/" + squareName)
-                                                                                             }
-                                                                       }
-                                                         },
-                                            TileSmall = new TileBinding
-                                                        {
-                                                            Branding = TileBranding.None,
-                                                            Content = new TileBindingContentAdaptive
-                                                                      {
-                                                                          BackgroundImage = new TileBackgroundImage
-                                                                                            {
-                                                                                                Source = new TileImageSource("ms-appdata:///local/" + squareSmallName)
-                                                                                            }
-                                                                      }
-                                                        }
-                                        }
-                           };
+                {
+                    Visual = new TileVisual
+                    {
+                        TileWide = new TileBinding
+                        {
+                            Branding = TileBranding.Name,
+                            Content = new TileBindingContentAdaptive
+                            {
+                                BackgroundImage = new TileBackgroundImage
+                                {
+                                    Source = "ms-appdata:///local/" + wideName
+                                }
+                            }
+                        },
+                        TileMedium = new TileBinding
+                        {
+                            Branding = TileBranding.Name,
+                            Content = new TileBindingContentAdaptive
+                            {
+                                BackgroundImage = new TileBackgroundImage
+                                {
+                                    Source = "ms-appdata:///local/" + squareName
+                                }
+                            }
+                        },
+                        TileSmall = new TileBinding
+                        {
+                            Branding = TileBranding.None,
+                            Content = new TileBindingContentAdaptive
+                            {
+                                BackgroundImage = new TileBackgroundImage
+                                {
+                                    Source = "ms-appdata:///local/" + squareSmallName
+                                }
+                            }
+                        }
+                    }
+                };
 
                 var doc = tile.GetXml();
                 var tileNotification = new TileNotification(doc);
@@ -116,12 +116,11 @@ namespace handyNews.Domain.Services
         }
 
         private static async Task<string> SaveFileAsync(RenderTargetBitmap image, string fileName, uint imageWidth,
-                                                        uint imageHeight)
+            uint imageHeight)
         {
-            var file =
-                await
-                    ApplicationData.Current.LocalFolder.CreateFileAsync(fileName,
-                                                                        CreationCollisionOption.ReplaceExisting);
+            var file = await ApplicationData.Current.LocalFolder.CreateFileAsync(fileName,
+                CreationCollisionOption.ReplaceExisting);
+
             using (var stream = await file.OpenAsync(FileAccessMode.ReadWrite))
             {
                 var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, stream);
@@ -129,12 +128,12 @@ namespace handyNews.Domain.Services
                 var bytes = pixelsBuffer.ToArray();
 
                 var dpi = DisplayInformation.GetForCurrentView()
-                                            .LogicalDpi;
+                    .LogicalDpi;
 
                 encoder.SetPixelData(BitmapPixelFormat.Bgra8,
-                                     BitmapAlphaMode.Straight,
-                                     (uint) image.PixelWidth, (uint) image.PixelHeight,
-                                     dpi, dpi, bytes);
+                    BitmapAlphaMode.Straight,
+                    (uint) image.PixelWidth, (uint) image.PixelHeight,
+                    dpi, dpi, bytes);
 
                 await encoder.FlushAsync();
                 stream.Dispose();
@@ -146,28 +145,19 @@ namespace handyNews.Domain.Services
         private async Task<RenderTargetBitmap> RenderAsync(FrameworkElement drawControl, int width, int height)
         {
             var frame = Window.Current.Content as Frame;
-            if (frame == null)
-            {
-                return null;
-            }
+            var page = frame?.Content as Page;
+            var grid = page?.Content as Grid;
 
-            var page = frame.Content as Page;
-            if (page == null)
-            {
-                return null;
-            }
-
-            var grid = page.Content as Grid;
             if (grid == null)
             {
                 return null;
             }
 
-            var canvas = grid.FindName(DrawCanvasName) as Canvas;
+            var canvas = grid.FindName(DRAW_CANVAS_NAME) as Canvas;
             if (canvas == null)
             {
                 canvas = new Canvas();
-                canvas.Name = DrawCanvasName;
+                canvas.Name = DRAW_CANVAS_NAME;
                 canvas.Opacity = 0;
                 canvas.Background = new SolidColorBrush(Colors.Transparent);
                 canvas.IsHitTestVisible = false;
